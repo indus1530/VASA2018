909a2918b8aa305c2d235aff932345f58362a2ab
SAJID LATIF <indus1530@gmail.com>
2022-09-07 19:40:04 +0500

    initialCommit

---
+++ app/src/main/java/com/sajidlatif/vasa2018/Other/templete.java
@@ -0,0 +1,15 @@
+package com.sajidlatif.vasa2018.Other;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sajidlatif.vasa2018.R;
+
+public class templete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kamran);
+    }
+}
