8cbeae7d1cd9b72312b4dd01a77aca7e982c7259
SAJID LATIF <indus1530@gmail.com>
2022-09-11 23:25:41 +0500

    AndroidX

---
+++ app/src/main/java/com/irfansyed/VAS/VASMonitring/Other/Genifno.java
@@ -0,0 +1,15 @@
+package com.irfansyed.VAS.VASMonitring.Other;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.irfansyed.VAS.VASMonitring.R;
+
+public class Genifno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.geninfo);
+    }
+}
