b1b7e491d77b7bc0d1ce58bbfa8b1d5258365350
SAJID LATIF <indus1530@gmail.com>
2022-10-24 13:42:44 +0500

    activity_section_a01.xml layoutChanges

--- app/src/main/java/edu/aku/abdulsajid/vasa2018/ui/SectionA01Activity.java
+++ app/src/main/java/edu/aku/abdulsajid/vasa2018/ui/SectionA01Activity.java
@@ -11,13 +11,13 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.validatorcrawler.aliazaz.Clear;
 import com.validatorcrawler.aliazaz.Validator;
 
 import edu.aku.abdulsajid.vasa2018.Other.globale;
 import edu.aku.abdulsajid.vasa2018.R;
 import edu.aku.abdulsajid.vasa2018.data.LocalDataManager;
 import edu.aku.abdulsajid.vasa2018.databinding.ActivitySectionA01Binding;
-import edu.aku.abdulsajid.vasa2018.utils.ClearAllcontrol;
 import edu.aku.abdulsajid.vasa2018.utils.InputFilterMinMax;
 
 public class SectionA01Activity extends AppCompatActivity {
@@ -59,21 +59,21 @@ public class SectionA01Activity extends AppCompatActivity {
 
         @Override
         public void afterTextChanged(Editable s) {
-            if (bi.edA4005.getText().hashCode() == s.hashCode()) {
-                if (bi.edA4005.getText().toString().trim().length() > 0 && Integer.parseInt(bi.edA4005.getText().toString().trim()) > 7) {
-                    bi.llA4006.setVisibility(View.GONE);
-                    ClearAllcontrol.ClearAll(bi.llA4006);
+            if (bi.a005.getText().hashCode() == s.hashCode()) {
+                if (bi.a005.getText().toString().trim().length() > 0 && Integer.parseInt(bi.a005.getText().toString().trim()) > 7) {
+                    bi.cvA006.setVisibility(View.GONE);
+                    Clear.clearAllFields(bi.cvA006);
                 } else {
-                    bi.llA4006.setVisibility(View.VISIBLE);
+                    bi.cvA006.setVisibility(View.VISIBLE);
                 }
             }
 
-            if (bi.edA4011.getText().hashCode() == s.hashCode()) {
-                if (bi.edA4011.getText().toString().trim().length() > 0 && Integer.parseInt(bi.edA4011.getText().toString().trim()) >= 1) {
-                    bi.llA4012.setVisibility(View.GONE);
-                    ClearAllcontrol.ClearAll(bi.llA4012);
+            if (bi.a011.getText().hashCode() == s.hashCode()) {
+                if (bi.a011.getText().toString().trim().length() > 0 && Integer.parseInt(bi.a011.getText().toString().trim()) >= 1) {
+                    bi.cvA012.setVisibility(View.GONE);
+                    Clear.clearAllFields(bi.cvA012);
                 } else {
-                    bi.llA4012.setVisibility(View.VISIBLE);
+                    bi.cvA012.setVisibility(View.VISIBLE);
                 }
             }
 
@@ -97,90 +97,77 @@ public class SectionA01Activity extends AppCompatActivity {
 
     void eventCall() {
 
-        bi.edA4005.addTextChangedListener(generalTextWatcher);
-        bi.edA4011.addTextChangedListener(generalTextWatcher);
+        bi.a005.addTextChangedListener(generalTextWatcher);
+        bi.a011.addTextChangedListener(generalTextWatcher);
 
-        //A4003
-        bi.a4003.setOnCheckedChangeListener((radioGroup, i) -> {
-            ClearAllcontrol.ClearAll(bi.llA4004);
-            ClearAllcontrol.ClearAll(bi.llA4005);
-            bi.llA4004.setVisibility(View.GONE);
-            bi.llA4005.setVisibility(View.GONE);
-            if (i == bi.rbA40031.getId()) {
-                bi.llA4004.setVisibility(View.VISIBLE);
-                bi.llA4005.setVisibility(View.VISIBLE);
+        //A003
+        bi.a003.setOnCheckedChangeListener((radioGroup, i) -> {
+            Clear.clearAllFields(bi.cvA004);
+            Clear.clearAllFields(bi.cvA005);
+            bi.cvA004.setVisibility(View.GONE);
+            bi.cvA005.setVisibility(View.GONE);
+            if (i == bi.a003a.getId()) {
+                bi.cvA004.setVisibility(View.VISIBLE);
+                bi.cvA005.setVisibility(View.VISIBLE);
             }
         });
 
-        //A4004
-        bi.a4004.setOnCheckedChangeListener((radioGroup, i) -> {
-            ClearAllcontrol.ClearAll(bi.llA4005);
-            bi.llA4005.setVisibility(View.GONE);
-            if (i == bi.rbA40041.getId() || i == bi.rbA40042.getId() || i == bi.rbA40043.getId()) {
-                bi.llA4005.setVisibility(View.VISIBLE);
+        //A004
+        bi.a004.setOnCheckedChangeListener((radioGroup, i) -> {
+            Clear.clearAllFields(bi.cvA005);
+            bi.cvA005.setVisibility(View.GONE);
+            if (i == bi.a004a.getId() || i == bi.a004b.getId() || i == bi.a004c.getId()) {
+                bi.cvA005.setVisibility(View.VISIBLE);
             }
         });
 
-        //A4007
-        bi.a4007.setOnCheckedChangeListener((radioGroup, i) -> {
-            ClearAllcontrol.ClearAll(bi.llA40071);
-            bi.llA40071.setVisibility(View.GONE);
-            if (i == bi.rbA40072.getId()) {
-                bi.llA40071.setVisibility(View.VISIBLE);
+        //A007a
+        bi.a007a.setOnCheckedChangeListener((radioGroup, i) -> {
+            Clear.clearAllFields(bi.cvA007b);
+            bi.cvA007b.setVisibility(View.GONE);
+            if (i == bi.a007b.getId()) {
+                bi.cvA007b.setVisibility(View.VISIBLE);
             }
         });
 
-        //A4009a
-        bi.a4009a.setOnCheckedChangeListener((radioGroup, i) -> {
-            ClearAllcontrol.ClearAll(bi.llA4010);
-            ClearAllcontrol.ClearAll(bi.llA4011);
-            ClearAllcontrol.ClearAll(bi.llA4012);
-            bi.llA4010.setVisibility(View.GONE);
-            bi.llA4011.setVisibility(View.GONE);
-            bi.llA4012.setVisibility(View.GONE);
-            if (i == bi.rbA4009a1.getId()) {
-                bi.llA4010.setVisibility(View.VISIBLE);
-                bi.llA4011.setVisibility(View.VISIBLE);
-                bi.llA4012.setVisibility(View.VISIBLE);
+        //A009
+        bi.a009.setOnCheckedChangeListener((radioGroup, i) -> {
+            Clear.clearAllFields(bi.cvA010);
+            Clear.clearAllFields(bi.cvA011);
+            Clear.clearAllFields(bi.cvA012);
+            bi.cvA010.setVisibility(View.GONE);
+            bi.cvA011.setVisibility(View.GONE);
+            bi.cvA012.setVisibility(View.GONE);
+            if (i == bi.a009a.getId()) {
+                bi.cvA010.setVisibility(View.VISIBLE);
+                bi.cvA011.setVisibility(View.VISIBLE);
+                bi.cvA012.setVisibility(View.VISIBLE);
             }
         });
 
-        //A4010
-        bi.a4010.setOnCheckedChangeListener((radioGroup, i) -> {
-            ClearAllcontrol.ClearAll(bi.llA4011);
-            ClearAllcontrol.ClearAll(bi.llA4012);
-            bi.llA4011.setVisibility(View.GONE);
-            bi.llA4012.setVisibility(View.GONE);
-            if (i == bi.rbA40101.getId()) {
-                bi.llA4011.setVisibility(View.VISIBLE);
-                bi.llA4012.setVisibility(View.VISIBLE);
-            } else if (i == bi.rbA40102.getId() || i == bi.rbA40103.getId() || i == bi.rbA40104.getId()) {
-                bi.llA4012.setVisibility(View.VISIBLE);
+        //A010
+        bi.a010.setOnCheckedChangeListener((radioGroup, i) -> {
+            Clear.clearAllFields(bi.cvA011);
+            Clear.clearAllFields(bi.cvA012);
+            bi.cvA011.setVisibility(View.GONE);
+            bi.cvA012.setVisibility(View.GONE);
+            if (i == bi.a010a.getId()) {
+                bi.cvA011.setVisibility(View.VISIBLE);
+                bi.cvA012.setVisibility(View.VISIBLE);
+            } else if (i == bi.a010b.getId() || i == bi.a010c.getId() || i == bi.a01096.getId()) {
+                bi.cvA012.setVisibility(View.VISIBLE);
             }
         });
 
-        //A4013u
-        bi.a4013u.setOnCheckedChangeListener((radioGroup, i) -> {
-            ClearAllcontrol.ClearAll(bi.llA4013d);
-            ClearAllcontrol.ClearAll(bi.llA4013m);
-            ClearAllcontrol.ClearAll(bi.llA4013y);
-            bi.llA4013d.setVisibility(View.GONE);
-            bi.llA4013m.setVisibility(View.GONE);
-            bi.llA4013y.setVisibility(View.GONE);
-            if (i == bi.rbA4013u1.getId()) bi.llA4013d.setVisibility(View.VISIBLE);
-            else if (i == bi.rbA4013u2.getId()) bi.llA4013m.setVisibility(View.VISIBLE);
-            else if (i == bi.rbA4013u3.getId()) bi.llA4013y.setVisibility(View.VISIBLE);
-        });
-
-        bi.edA4005.setFilters(new InputFilter[]{new InputFilterMinMax(0, 22, 99, 99)});
-        bi.edA4013d.setFilters(new InputFilter[]{new InputFilterMinMax(0, 29, 99, 99)});
-        bi.edA4013m.setFilters(new InputFilter[]{new InputFilterMinMax(1, 11, 99, 99)});
-        bi.edA4013y.setFilters(new InputFilter[]{new InputFilterMinMax(1, 49, 99, 99)});
+        bi.a005.setFilters(new InputFilter[]{new InputFilterMinMax(0, 22, 99, 99)});
+        bi.a013d.setFilters(new InputFilter[]{new InputFilterMinMax(0, 29, 99, 99)});
+        bi.a013m.setFilters(new InputFilter[]{new InputFilterMinMax(1, 11, 99, 99)});
+        bi.a013y.setFilters(new InputFilter[]{new InputFilterMinMax(1, 49, 99, 99)});
 
     }
 
 
-    public void btnContinue(View view) {
+    public void btnContinue() {
         if (!validateField()) return;
         value_assignment();
         insert_data();
@@ -194,7 +181,7 @@ public class SectionA01Activity extends AppCompatActivity {
 
         study_id = bi.studyid.getText().toString().length() > 0 ? bi.studyid.getText().toString().trim() : "0";
 
-        A4001 = bi.a4001.getText().toString().trim().length() > 0 ? bi.a4001.getText().toString().trim() : "-1";
+        A4001 = bi.a001.getText().toString().trim().length() > 0 ? bi.a001.getText().toString().trim() : "-1";
 
         A4002 = bi.a002a.isChecked() ? "1"
                 : bi.a002b.isChecked() ? "2"
@@ -206,79 +193,74 @@ public class SectionA01Activity extends AppCompatActivity {
                 : bi.a00299.isChecked() ? "99"
                 : "-1";
 
-        A4003 = bi.rbA40031.isChecked() ? "1"
-                : bi.rbA40032.isChecked() ? "2"
-                : bi.rbA4003DK.isChecked() ? "9"
-                : bi.rbA4003RA.isChecked() ? "8"
+        A4003 = bi.a003a.isChecked() ? "1"
+                : bi.a003b.isChecked() ? "2"
+                : bi.a00398.isChecked() ? "98"
+                : bi.a00399.isChecked() ? "99"
                 : "-1";
 
-        A4004 = bi.rbA40041.isChecked() ? "1"
-                : bi.rbA40042.isChecked() ? "2"
-                : bi.rbA40043.isChecked() ? "3"
-                : bi.rbA4004DK.isChecked() ? "9"
-                : bi.rbA4004RA.isChecked() ? "8"
+        A4004 = bi.a004a.isChecked() ? "1"
+                : bi.a004b.isChecked() ? "2"
+                : bi.a00498.isChecked() ? "98"
+                : bi.a00499.isChecked() ? "99"
                 : "-1";
 
-        A4005 = bi.edA4005.getText().toString().trim().length() > 0 ? bi.edA4005.getText().toString().trim() : "-1";
+        A4005 = bi.a005.getText().toString().trim().length() > 0 ? bi.a005.getText().toString().trim() : "-1";
 
-        A4006 = bi.rbA40061.isChecked() ? "1"
-                : bi.rbA40062.isChecked() ? "2"
-                : bi.rbA4006DK.isChecked() ? "9"
-                : bi.rbA4006RA.isChecked() ? "8"
+        A4006 = bi.a006a.isChecked() ? "1"
+                : bi.a006b.isChecked() ? "2"
+                : bi.a00698.isChecked() ? "98"
+                : bi.a00699.isChecked() ? "99"
                 : "-1";
 
-        A4007 = bi.rbA40071.isChecked() ? "1"
-                : bi.rbA40072.isChecked() ? "2"
-                : bi.rbA40073.isChecked() ? "3"
-                : bi.rbA40074.isChecked() ? "4"
-                : bi.rbA40075.isChecked() ? "5"
-                : bi.rbA40076.isChecked() ? "6"
-                : bi.rbA4007DK.isChecked() ? "9"
-                : bi.rbA4007RA.isChecked() ? "8"
+        A4007 = bi.a007aa.isChecked() ? "1"
+                : bi.a007ab.isChecked() ? "2"
+                : bi.a007ac.isChecked() ? "3"
+                : bi.a007ad.isChecked() ? "4"
+                : bi.a007ae.isChecked() ? "5"
+                : bi.a007a96.isChecked() ? "96"
+                : bi.a007a98.isChecked() ? "98"
+                : bi.a007a99.isChecked() ? "99"
+                : "-1";
+        A40071 = bi.a007b.getText().toString().trim().length() > 0 ? bi.a007b.getText().toString().trim() : "-1";
+
+        A4008 = bi.a008a.isChecked() ? "1"
+                : bi.a008b.isChecked() ? "2"
+                : bi.a00898.isChecked() ? "98"
+                : bi.a00899.isChecked() ? "99"
                 : "-1";
 
-        A40071 = bi.edA40071.getText().toString().trim().length() > 0 ? bi.edA40071.getText().toString().trim() : "-1";
-
-        A4008 = bi.rbA40081.isChecked() ? "1"
-                : bi.rbA40082.isChecked() ? "2"
-                : bi.rbA4008DK.isChecked() ? "9"
-                : bi.rbA4008RA.isChecked() ? "8"
+        A4009a = bi.a009a.isChecked() ? "1"
+                : bi.a009b.isChecked() ? "2"
+                : bi.a00998.isChecked() ? "98"
+                : bi.a00999.isChecked() ? "99"
                 : "-1";
 
-        A4009a = bi.rbA4009a1.isChecked() ? "1"
-                : bi.rbA4009a2.isChecked() ? "2"
-                : bi.rbA4009aDK.isChecked() ? "9"
-                : bi.rbA4009aRA.isChecked() ? "8"
+        A4010 = bi.a010a.isChecked() ? "1"
+                : bi.a010b.isChecked() ? "2"
+                : bi.a010c.isChecked() ? "3"
+                : bi.a01096.isChecked() ? "96"
+                : bi.a01098.isChecked() ? "98"
+                : bi.a01099.isChecked() ? "99"
                 : "-1";
 
-        A4010 = bi.rbA40101.isChecked() ? "1"
-                : bi.rbA40102.isChecked() ? "2"
-                : bi.rbA40103.isChecked() ? "3"
-                : bi.rbA40104.isChecked() ? "4"
-                : bi.rbA4010DK.isChecked() ? "9"
-                : bi.rbA4010RA.isChecked() ? "8"
+        A4011 = bi.a011.getText().toString().trim().length() > 0 ? bi.a011.getText().toString().trim() : "-1";
+
+        A4012 = bi.a012.getText().toString().trim().length() > 0 ? bi.a012.getText().toString().trim() : "-1";
+
+        A4013u = bi.a013d.isChecked() ? "1"
+                : bi.a013m.isChecked() ? "2"
+                : bi.a013y.isChecked() ? "3"
+                : bi.a01398.isChecked() ? "98"
+                : bi.a01399.isChecked() ? "99"
                 : "-1";
+        A4013d = bi.a013dx.getText().toString().trim().length() > 0 ? bi.a013dx.getText().toString().trim() : "-1";
+        A4013m = bi.a013mx.getText().toString().trim().length() > 0 ? bi.a013mx.getText().toString().trim() : "-1";
+        A4013y = bi.a013yx.getText().toString().trim().length() > 0 ? bi.a013yx.getText().toString().trim() : "-1";
 
-        A4011 = bi.edA4011.getText().toString().trim().length() > 0 ? bi.edA4011.getText().toString().trim() : "-1";
-
-        A4012 = bi.edA4012.getText().toString().trim().length() > 0 ? bi.edA4012.getText().toString().trim() : "-1";
-
-        A4013u = bi.rbA4013u1.isChecked() ? "1"
-                : bi.rbA4013u2.isChecked() ? "2"
-                : bi.rbA4013u3.isChecked() ? "3"
-                : bi.rbA4013uDK.isChecked() ? "9"
-                : bi.rbA4013uRA.isChecked() ? "8"
-                : "-1";
-
-        A4013d = bi.edA4013d.getText().toString().trim().length() > 0 ? bi.edA4013d.getText().toString().trim() : "-1";
-
-        A4013m = bi.edA4013m.getText().toString().trim().length() > 0 ? bi.edA4013m.getText().toString().trim() : "-1";
-
-        A4013y = bi.edA4013y.getText().toString().trim().length() > 0 ? bi.edA4013y.getText().toString().trim() : "-1";
-
-        A4014 = bi.rbA40141.isChecked() ? "1"
-                : bi.rbA40142.isChecked() ? "2"
-                : bi.rbA4014DK.isChecked() ? "9"
+        A4014 = bi.a014a.isChecked() ? "1"
+                : bi.a014b.isChecked() ? "2"
+                : bi.a01498.isChecked() ? "98"
                 : "-1";
 
         STATUS = "0";
